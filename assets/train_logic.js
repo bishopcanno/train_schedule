4d47bb953ece86ace7275f7c487f25c56688c42d
bishopcanno <ericbispres@gmail.com>
2018-09-14 20:35:56 -0600

    All functionality is working except if the first arrival time is later than the current time. Table does populate dymanically. Parallax and all functions are working correctly. Styling is done mostly by materialize this time rather than bootstrap. Lastly there may be a way for me to cleanup the look of minCalculator and tableDisplay functions in regards to how they utilize arguments and variables. specifically i have it coded currently that the argument is immediately turned into a variable and then i use that variable. May not need to go that route and can just use the variable without initializing a variable

--- assets/train_logic.js
+++ assets/train_logic.js
@@ -1,3 +1,4 @@
+// database initialization and setup
 var config = {
     apiKey: "AIzaSyBr3rq3hytpOQhA2dK_txJ5AKYD2RQaRYU",
     authDomain: "fir-alpha-omega.firebaseapp.com",
@@ -6,78 +7,106 @@ var config = {
     storageBucket: "fir-alpha-omega.appspot.com",
     messagingSenderId: "192089995703"
   };
- 
-  // Get a reference to the database service
-  firebase.initializeApp(config);
-  // Create a variable to reference the database
-  var database = firebase.database();
- 
- //on click function to generate new line in table
+firebase.initializeApp(config);
+var database = firebase.database();
+// will be used by timeCheck to push a true or false and will be later used by minCalculator to run and if else if
+var arrivalBeforePresentTime = "";
+// initializes the current time and formats one into military
+var now = moment();
+var nowMilitary = moment().format("HH:mm")
+// these variables will be changed based on the if else if run in minCalculator and then passed into tableDisplay
+var minAway = "";
+var arrivalTime = "";
+// gives parallax functionality
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+// function that uses an if else if to pass back true or false to the arrivalBeforePresentTime var
+function timeCheck(Z, Y){      
+    if (Z > Y){
+        arrivalBeforePresentTime = true;
+    } else if (Z <= Y){
+        arrivalBeforePresentTime = false;
+    };
+};
+// function for all the calculation and necassary var manipulation that will be based into tableDisplay function
+function minuteCalculator(A){
+    // will recieve the database object as an argument and creates a new variable... for cleanliness i could change all instances of that variable to A
+    var trainObjectMinute = A;
+    // converts the arrival variable from the object into military time set one year ago and pulls the frequency variale out of the object and converts it to a variable
+    var newDateConverted = moment(trainObjectMinute.val().arrival, "HH:mm").subtract(1, "years");
+    var trainFreq = trainObjectMinute.val().frequency;
+    // finds the difference in time between the current time and the newDateConverted variable/ formats that into military time for functionality
+    var diffTime = moment().diff(moment(newDateConverted), "minutes");
+    var diffTimeFormated = moment(diffTime ).format('HH:mm');
+    // finds the remainder of the difference between difftime and trainfreq and then subtracts that number from trainFreq after initializing a var
+    var tRemainder = diffTime % trainFreq;
+    var tMinutesTillTrain = trainFreq - tRemainder;
+    // if tRemainder is zero then the min till next train will display boarding on the table
+    if (tRemainder === 0) {
+        tMinutesTillTrain = "BOARDING";
+    }
+    // uses if else if to determine if what to set arrivalTime and minAway variables to
+    if(arrivalBeforePresentTime){
+        arrivalTime = moment(trainObjectMinute.val().arrival).format("HH:mm");
+        minAway = diffTimeFormated;
+    } else if (!arrivalBeforePresentTime){
+        arrivalTime = moment().add(tMinutesTillTrain, 'minutes').format('HH:mm')
+        minAway = tMinutesTillTrain;
+    };
+    // calls the tableDisplay object and pass it the database object that minCalculator recieved 
+    tableDislpy(trainObjectMinute);   
+};
+// function that will dynamically change the display table on the html page with what the user imputs
+function tableDislpy(B){
+    // will recieve the database object as an argument and creates a new variable... for cleanliness i could change all instances of that variable to B
+    var trainObjectDisplay = B;
+    // creates a new table row element
+    var newLine = $("<tr>");
+    // these variables will be displayed by creating new table colomn elements and then displaying text of the variables they are fed
+    var displayName = $("<td>").text(trainObjectDisplay.val().name);
+    var displayDestination = $("<td>").text(trainObjectDisplay.val().destination);
+    var displayFreq = $("<td>").text(trainObjectDisplay.val().frequency);
+    // these next two are manipulated by minCalculator based on if arrivalBeforePresentTime is true or false
+    var displayArrival = $("<td>").text(arrivalTime);
+    var displayMinAway = $("<td>").text(minAway);
+    // displays the full time and date
+    var displayUTC =$("<td>").text(now);
+    // appends the above variables to the newLine variable
+    newLine.append(displayName);
+    newLine.append(displayDestination);
+    newLine.append(displayFreq);
+    newLine.append(displayArrival);
+    newLine.append(displayMinAway)
+    newLine.append(displayUTC);
+    // jquery hooks into the trainInfoDisplay which is the table body then appends variable newLine
+    $("#trainInfoDisplay").append(newLine)
+};
+// listens for the click on the add button and then gathers the users inputs and passes them to the database
 $("#add").on("click", function() {
     event.preventDefault();
-
+    // grabs the user inputs from the form 
     var name = $("#nameForm").val().trim()
     var destination = $("#destForm").val().trim()
     var arrival = $("#timeForm").val().trim()
     var frequency = $("#freqForm").val().trim()
-
-    timeCalculation(arrival, frequency);
-
-    // var arrival = moment.unix(startDate).format("MM/DD/YYYY")
-
-    // var empMonths = moment().diff(moment(startDate, "X"), "months");
-
-
+    // resets the form values to blank 
+    $("#nameForm").val("");
+    $("#destForm").val("");
+    $("#timeForm").val("");
+    $("#freqForm").val("");
+    // pushes the user's inputs to the database
     database.ref().push({
         name: name,
         destination: destination,
         arrival: arrival,
         frequency: frequency,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
+    });   
 });
-// ___________________________________________________________________________________________________________________________________________
- //listening event when data changes in database. undates html
- database.ref().on("child_added", function(childSnapshot){
-    
-    console.log(childSnapshot.val().name)
-    console.log(childSnapshot.val().destination)
-    console.log(childSnapshot.val().arrival)
-    console.log(childSnapshot.val().frequency)
- 
-    // console.log(moment(childSnapshot.val().startDate).diff(moment(), "months"));
- 
-    // var months = moment(childSnapshot.val().startDate).diff(moment(), "months") * -1
- 
-    // var total = months * childSnapshot.val().monthlyRate
- 
-    var newLine = $("<tr>")
-    
- 
-    var newName = $("<td>").text(childSnapshot.val().name);
-    var newDestination = $("<td>").text(childSnapshot.val().destination);
-    var newFreq = $("<td>").text(childSnapshot.val().frequency);
-    var newArrival = $("<td>").text("placeholder");
-    var minAway = $("<td>").text("placeholder")
-   
- 
-    newLine.append(newName)
-    newLine.append(newDestination)
-    newLine.append(newFreq)
-    newLine.append(newArrival)
-    newLine.append(minAway)
-    
- 
-    $("#trainInfoDisplay").append(newLine)
- 
-    $("#nameForm").val("");
-    $("#destForm").val("");
-    $("#timeForm").val("");
-    $("#freqForm").val("");
- });
-
- function timeCalculation(x , y){
-     
-
- };
-
+// whenever something is added to the database this runs
+database.ref().on("child_added", function(trainDataBaseObject){
+    // calls the timeCheck function and passes the arrival variable from the database object and the nowMilitary variable as arguments
+    timeCheck(trainDataBaseObject.val().arrival, nowMilitary);
+    // calls the minuteCalculator and passes the data base object as an argument
+    minuteCalculator(trainDataBaseObject);
+});
